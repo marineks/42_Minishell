da9c70ec757010ef698c9085d6ddbf1df1e2c4fb
Marine SANJUAN <marine@sanjuan.eu>
2022-02-13 17:15:11 +0100

    documented grammar function and 42norm ok

--- srcs/00_lexer/grammar.c
+++ srcs/00_lexer/grammar.c
@@ -1,27 +1,36 @@
 #include "minishell.h"
 
 /**
- * @brief 
+ * @brief Checks whether there are two consecutive operators or more in the 
+ * 		  chained list of tokens.
+ * 		  Small exception : the case where the operators are PIPE and GREATER
+ *		  i.e "echo coucou |>test1.txt"
+		  Nb: Why >= 4 ? Because the operators' enums begin from PIPE (4).
+ * @param tk_node: A pointer to a node of the linked list of tokens.
  * 
- * @param tk_node 
- * @return int 
  */
 static int	check_consecutive_ops(t_token *tk_node)
 {
 	if (tk_node->next)
 	{
-		if (tk_node->type == PIPE && tk_node->next->type == GREATER)	// cas "echo coucou |>test1.txt"
+		if (tk_node->type == PIPE && tk_node->next->type == GREATER)
 			return (FALSE);
-		else if ((tk_node->type >= 4 && tk_node->next->type >= 4) 
+		else if ((tk_node->type >= 4 && tk_node->next->type >= 4)
 			&& tk_node->next->type != END)
 			return (TRUE);
 	}
 	return (FALSE);
 }
 
+/**
+ * @brief 
+ * 
+ * @param tk_list 
+ * 
+ */
 int	apply_grammar(t_token **tk_list)
 {
-	t_token *tmp;
+	t_token	*tmp;
 
 	tmp = *tk_list;
 	while (tmp)
--- srcs/00_lexer/specify_token.c
+++ srcs/00_lexer/specify_token.c
@@ -3,7 +3,7 @@
 /**
 *	@brief  Checks whether there is a '$' in the WORD-typed token and
 *		    changes the type of said token to VAR accordingly.
-*	@param  tk_node : A pointer to a pointer on a node in chained list t_token
+*	@param  tk_node: A pointer to a pointer on a node in chained list t_token
 *
 */
 static void	check_var_type(t_token **tk_node)
@@ -40,7 +40,7 @@ static void	check_state_value(t_token **tk_node)
 /**
 *	@brief  After tokenising the user's command line, this function aims at
 *			further specifying the tokens before eventually expanding them.
-*	@param  tk_list : A pointer to a pointer to the chained list t_token
+*	@param  tk_list: A pointer to a pointer to the chained list t_token
 *
 */
 int	specify(t_token **tk_list)
