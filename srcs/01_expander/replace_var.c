b3df1acbf0f03795dc61ab9f74c4cad701d96f1f
Marine SANJUAN <marine@sanjuan.eu>
2022-02-17 14:41:45 +0100

    implemented the function replace_var (when there is only one variable in the token)

--- srcs/01_expander/replace_var.c
+++ srcs/01_expander/replace_var.c
@@ -9,8 +9,7 @@ int	erase_var(t_token **tk_node, char *str)
 
 	i = 0;
 	j = 0;
-	len = ft_strlen(str) - count_len_var(str); // PB QUAND STR EST NE VAR QUIEXISTE PAS! On va lpus loin car +1 lign 20
-	printf("len : %d\n", len);
+	len = ft_strlen(str) - count_len_var(str);
 	new_str = (char *)malloc(sizeof(char) * len);
 	if (!new_str)
 		return (FAILURE);
@@ -33,12 +32,66 @@ int	erase_var(t_token **tk_node, char *str)
 	return (SUCCESS);
 }
 
+int	erase_and_replace(t_token **tk_node, char *str, char *var_value)
+{
+	int i;
+	int j;
+	int	k;
+	int len;
+	char *new_str;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	len = ft_strlen(str) - count_len_var(str) + ft_strlen(var_value);
+	printf("len : %d\n", len);
+	new_str = (char *)malloc(sizeof(char) * len);
+	if (!new_str)
+		return (FAILURE);
+	while (str[i])
+	{
+		if (str[i] == '$')
+		{
+			while (var_value[k])
+			{
+				new_str[j] = var_value[k];
+				k++;
+				j++;
+			}
+			i = i + count_len_var(str) + 1; // pour skip le $
+			if (str[i] == '\0')
+				break ;
+			
+		}
+		new_str[j] = str[i];
+		i++;
+		j++;
+	}
+	new_str[j] = '\0';
+	free((*tk_node)->str);
+	(*tk_node)->str = new_str;
+	printf("str dans le nod : %s\n", (*tk_node)->str);
+	return (SUCCESS);
+}
+
 int	replace_var(t_token **tk_node, char *var_value)
 {
 	if (var_value == NULL)
 	{
 		if (erase_var(tk_node, (*tk_node)->str) == FAILURE)
+		{
+			free(var_value);
 			return (FAILURE);
+		}
+			
+	}
+	else
+	{
+		if (erase_and_replace(tk_node, (*tk_node)->str, var_value) == FAILURE)
+		{
+			free(var_value);
+			return (FAILURE);
+		}
 	}
 	free(var_value);
 	return (SUCCESS);
