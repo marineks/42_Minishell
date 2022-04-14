89a9c87b1e61f870e86e201db1b1126e82b29451
Marine Sanjuan <marine@sanjuan.eu>
2022-04-15 00:31:04 +0200

    fixed segfault in get_echo function by protecting the cmd->infos.flags, in the case where only the cmd 'echo' is issued

--- srcs/03_builtins/00_echo.c
+++ srcs/03_builtins/00_echo.c
@@ -15,17 +15,20 @@ void	get_echo(t_cmd *cmd)
 
 	print_newline = true;
 	i = 0;
-	if (ft_strcmp("-n", cmd->infos.flags[i]) == SUCCESS)
+	if (cmd->infos.flags)
 	{
-		print_newline = false;
-		i++;
-	}
-	while (cmd->infos.flags[i])
-	{
-		len = ft_strlen(cmd->infos.flags[i]);
-		write(cmd->infos.fd_out, cmd->infos.flags[i], len);
-		write(cmd->infos.fd_out, " ", 1);
-		i++;
+		if (ft_strcmp("-n", cmd->infos.flags[i]) == SUCCESS)
+		{
+			print_newline = false;
+			i++;
+		}
+		while (cmd->infos.flags[i])
+		{
+			len = ft_strlen(cmd->infos.flags[i]);
+			write(cmd->infos.fd_out, cmd->infos.flags[i], len);
+			write(cmd->infos.fd_out, " ", 1);
+			i++;
+		}
 	}
 	if (print_newline == true)
 		write(cmd->infos.fd_out, "\n", 1);
