53a16b22b0a4748c2e9058409d75d3ff58a872ad
Marine Sanjuan <marine@sanjuan.eu>
2022-04-14 23:55:00 +0200

    env builtin coded - no leaks and 42 norm compliant

--- srcs/03_builtins/05_env.c
+++ srcs/03_builtins/05_env.c
@@ -1,1 +1,18 @@
 #include "minishell.h"
+
+/**
+ * @brief Display the env variables and values on the terminal
+ * 
+ * @param env our chained list of env variables
+ */
+void	get_env(t_env *env)
+{
+	t_env	*tmp;
+
+	tmp = env;
+	while (tmp->next)
+	{
+		printf("%s\n", tmp->line);
+		tmp = tmp->next;
+	}
+}
