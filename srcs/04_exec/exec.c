8a9f1f78d197bcea61414bb6d6058674ad6eddaf
Tiffany Manolis <tiffany.mano@gmail.com>
2022-04-24 13:46:23 +0200

    fonction pipe in progress : try to set up good fd to write or read from pipe in case of redir

--- srcs/04_exec/exec.c
+++ srcs/04_exec/exec.c
@@ -26,19 +26,39 @@ int	exec_one_cmd(t_data *data, t_cmd *cmd)
 	return (SUCCESS);
 }
 
+int	exec_pipe(t_cmd *cmd, int pipe_fd_in)
+{
+	int		tube_fd[2];
+
+	if (pipe(tube_fd) == -1)
+	{
+        perror("Pipe");
+        return (FAILURE);
+    }
+	if (cmd->infos.redir_in == true)
+		dup2(tube_fd[READ], cmd->infos.fd_in);
+	if (cmd->infos.redir_out == true)
+		dup2(tube_fd[WRITE], cmd->infos.fd_out);
+	else
+	{
+		dup2(tube_fd[1], STDOUT_FILENO);
+		close(tube_fd[0]);
+	}
+}
+
 int	exec(t_data *data, t_cmd *cmd_lst, int pipe_fd_in)
 {
 	(void)pipe_fd_in;
-	
+
 	if (cmd_lst->right == NULL)
 		return (exec_one_cmd(data, cmd_lst));
-	// else if (cmd_lst->is_pipe == true)
-	// {
-	// 	exec_pipe(cmd_lst, pipe_fd_in);
-	// 	// if (cmd_lst->infos.builtin == true)
-	// 	// 	exec_built_in();
-	// 	// else
-	// 	// 	exec_cmd();
-	// }
+	else if (cmd_lst->is_pipe == true)
+	{
+		exec_pipe(cmd_lst, pipe_fd_in);
+		// if (cmd_lst->infos.builtin == true)
+		// 	exec_built_in();
+		// else
+		// 	exec_cmd();
+	}
 	return (SUCCESS);
 }
