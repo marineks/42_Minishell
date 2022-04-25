033791f3fa3f2f90bae77a871ef1a601ea558b15
Marine SANJUAN <marine@sanjuan.eu>
2022-04-25 13:59:19 +0200

    fixed error of parsing with echo$\? by touching up the echo function a bit

--- srcs/03_builtins/00_echo.c
+++ srcs/03_builtins/00_echo.c
@@ -1,5 +1,49 @@
 #include "minishell.h"
 
+static int	find_str(char *haystack, char *needle)
+{
+	int	i;
+
+	i = 0;
+	while (haystack[i])
+	{
+		if (haystack[i] == needle[0])
+		{
+			if (haystack[i + 1] && haystack[i + 1] == needle[1])
+				return (SUCCESS);
+		}
+		i++;
+	}
+	return (FAILURE);
+}
+
+static char	*replace_exit_status(char *flags)
+{
+	char	*exit_value;
+	char	*new_str;
+	int		i;
+	int		j;
+	int		len;
+
+	exit_value = ft_itoa(g_exit_status);
+	len = ft_strlen(exit_value) + 1;
+	new_str = malloc(sizeof(char) * (ft_strlen(flags) - 2 + len));
+	i = -1;
+	while (flags[++i] && flags[i] != '$')
+		new_str[i] = flags[i];
+	len = i + 2;
+	j = 0;
+	while (exit_value[j])
+		new_str[i++] = exit_value[j++];
+	j = 0;
+	while (flags[len])
+		new_str[i++] = flags[len++];
+	new_str[i] = '\0';
+	free(flags);
+	free(exit_value);
+	return (new_str);
+}
+
 /**
  * @brief The command "echo" prints a string. "echo -n" does not print the final
  * 		  newline. 
@@ -15,25 +59,19 @@ void	get_echo(t_cmd *cmd)
 
 	print_newline = true;
 	i = 0;
-	if (cmd->infos.flags)
+	if (cmd->infos.flags && ft_strcmp("-n", cmd->infos.flags[i]) == SUCCESS)
 	{
-		if (ft_strcmp("-n", cmd->infos.flags[i]) == SUCCESS)
-		{
-			print_newline = false;
-			i++;
-		}
-		while (cmd->infos.flags[i])
-		{
-			if (ft_strcmp("$?", cmd->infos.flags[i]) == SUCCESS)
-			{
-				free(cmd->infos.flags[i]);
-				cmd->infos.flags[i] = ft_itoa(g_exit_status);
-			}
-			len = ft_strlen(cmd->infos.flags[i]);
-			write(cmd->infos.fd_out, cmd->infos.flags[i], len);
-			write(cmd->infos.fd_out, " ", 1);
-			i++;
-		}
+		print_newline = false;
+		i++;
+	}
+	while (cmd->infos.flags && cmd->infos.flags[i])
+	{
+		if (find_str(cmd->infos.flags[i], "$?") == SUCCESS)
+			cmd->infos.flags[i] = replace_exit_status(cmd->infos.flags[i]);
+		len = ft_strlen(cmd->infos.flags[i]);
+		write(cmd->infos.fd_out, cmd->infos.flags[i], len);
+		write(cmd->infos.fd_out, " ", 1);
+		i++;
 	}
 	if (print_newline == true)
 		write(cmd->infos.fd_out, "\n", 1);
