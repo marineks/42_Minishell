5c9d2df74e2856fd81b37ec1ff3594903433401c
Tiffany MANOLIS <tiffany.mano@gmail.com>
2022-04-26 15:13:25 +0200

    clean Brazil

--- srcs/05_utils/brazil.c
+++ srcs/05_utils/brazil.c
@@ -1,33 +1,10 @@
 #include "minishell.h"
 
-void	free_t_pipe(t_data *data)
-{
-	t_list *tmp;
-
-	if (data->pipe)
-	{
-		if (data->pipe->lst)
-		{
-			while (data->pipe->lst)
-			{
-				tmp = data->pipe->lst;
-				data->pipe->lst = data->pipe->lst->next;
-				free(tmp);
-			}
-			data->pipe->lst = NULL;
-		}
-		else
-			(void) tmp;
-		free(data->pipe);
-	}
-}
-
 void	escape_to_brazil(t_data *data)
 {
 	if (data->line)
 		free(data->line);
 	if (data->token)
 		ft_lstclear_token(&data->token, &free);
-	// free_t_pipe(data);
 	rl_clear_history ();
 }
