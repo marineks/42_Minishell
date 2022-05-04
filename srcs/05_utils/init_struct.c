f1f96024c36c1a6a8754fbada69542a45befb077
Tiffany MANOLIS <tiffany.mano@gmail.com>
2022-05-04 15:10:49 +0200

    sort export fixed

--- srcs/05_utils/init_struct.c
+++ srcs/05_utils/init_struct.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_struct.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tmanolis <tmanolis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 17:20:27 by msanjuan          #+#    #+#             */
-/*   Updated: 2022/05/04 12:53:23 by msanjuan         ###   ########.fr       */
+/*   Updated: 2022/05/04 15:09:04 by tmanolis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,12 +74,27 @@ t_env	*stock_envp_in_chained_list(char **envp)
 	return (env_list);
 }
 
+static void	replace_and_sort(t_env *current, t_env *next_node)
+{
+	char	*tmp_line;
+	char	*tmp_var;
+	char	*tmp_val;
+
+	tmp_line = current->line;
+	tmp_var = current->var_name;
+	tmp_val = current->var_value;
+	current->line = next_node->line;
+	current->var_name = next_node->var_name;
+	current->var_value = next_node->var_value;
+	next_node->line = tmp_line;
+	next_node->var_name = tmp_var;
+	next_node->var_value = tmp_val;
+}
+
 void	sort_export(t_env *env_export)
 {
 	t_env	*next_node;
 	t_env	*current;
-	char	*tmp_var;
-	char	*tmp_val;
 
 	current = env_export;
 	while (current != NULL)
@@ -88,15 +103,9 @@ void	sort_export(t_env *env_export)
 		while (next_node != NULL)
 		{
 			if (current->var_name[0] > next_node->var_name[0])
-			{	
-				tmp_var = current->var_name;
-				tmp_val = current->var_value;
-				current->var_name = next_node->var_name;
-				current->var_value = next_node->var_value;
-				next_node->var_name = tmp_var;
-				next_node->var_value = tmp_val;
-			}
-			next_node = next_node->next;
+				replace_and_sort(current, next_node);
+			else
+				next_node = next_node->next;
 		}
 		current = current->next;
 	}
